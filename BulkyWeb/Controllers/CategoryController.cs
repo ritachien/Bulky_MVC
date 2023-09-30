8ba0a533f329e0fb957c33617172eb2e693fc15b
Rita Chien <ritachien929@gmail.com>
2023-09-30 16:17:53 +0800

    feat: add category view page

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers;
+
+public class CategoryController: Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
