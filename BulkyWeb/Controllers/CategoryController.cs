c3747f9ca5ec7bb6aa74311f328e902e00207e87
Rita Chien <ritachien929@gmail.com>
2023-09-30 18:11:41 +0800

    feat: implement create category feature

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers;
@@ -14,5 +15,18 @@ public class CategoryController: Controller
     {
         var objCategoryList = _db.Categories.ToList();
         return View(objCategoryList);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult Create(Category obj)
+    {
+        _db.Categories.Add(obj);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
     }
 }
