ead8692a032da9c309904bf0e6213822d709c114
Rita Chien <ritachien929@gmail.com>
2023-09-30 17:13:30 +0800

    feat: display categories in page

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyWeb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers;
 
 public class CategoryController: Controller
 {
+    private readonly ApplicationDbContext _db;
+    public CategoryController(ApplicationDbContext db)
+    {
+        this._db = db;
+    }
     public IActionResult Index()
     {
-        return View();
+        var objCategoryList = _db.Categories.ToList();
+        return View(objCategoryList);
     }
 }
