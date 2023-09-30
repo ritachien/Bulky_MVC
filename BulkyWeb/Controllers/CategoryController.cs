59499e0f1ffec78a4fb28a5a91ff241f86aba145
Rita Chien <ritachien929@gmail.com>
2023-09-30 18:44:02 +0800

    feat: add server side validation for create category

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,16 +1,20 @@
 ﻿using BulkyWeb.Data;
 using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.IdentityModel.Tokens;
 
 namespace BulkyWeb.Controllers;
 
-public class CategoryController: Controller
+public class CategoryController : Controller
 {
     private readonly ApplicationDbContext _db;
+
     public CategoryController(ApplicationDbContext db)
     {
-        this._db = db;
+        _db = db;
     }
+
     public IActionResult Index()
     {
         var objCategoryList = _db.Categories.ToList();
@@ -21,12 +25,20 @@ public class CategoryController: Controller
     {
         return View();
     }
-    
+
     [HttpPost]
     public IActionResult Create(Category obj)
     {
+        if (obj.Name == obj.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the name.");
+        }
+        
+        if (!ModelState.IsValid) return View(obj);
+        
         _db.Categories.Add(obj);
         _db.SaveChanges();
         return RedirectToAction("Index");
+
     }
 }
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http.Features;
 
 namespace BulkyWeb.Models;
 
@@ -8,8 +9,10 @@ public class Category
     [Key]
     public int Id { get; set; }
     [Required]
+    [MaxLength(30)]
     [DisplayName("Category Name")]
     public string Name { get; set; }
     [DisplayName("Display Order")]
+    [Range(1, 100, ErrorMessage = "Display order should between 1-100")]
     public int DisplayOrder { get; set; }
 }
