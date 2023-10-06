cd5ad36187399f7065c610627aa5fbadf4008ead
Rita Chien <ritachien929@gmail.com>
2023-10-06 23:22:22 +0800

    feat: implement product repository

---
+++ BulkyBook.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,8 @@
+﻿using BulkyBook.Models.Models;
+
+namespace BulkyBook.DataAccess.Repository.IRepository;
+
+public interface IProductRepository : IRepository<Product>
+{
+    void Update(Product obj);
+}
--- BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -7,10 +7,13 @@ public class UnitOfWork : IUnitOfWork
 {
     private readonly ApplicationDbContext _db;
     public ICategoryRepository Category { get; private set; }
+    public IProductRepository Product { get; }
+
     public UnitOfWork(ApplicationDbContext db)
     {
         _db = db;
         Category = new CategoryRepository(_db);
+        Product = new ProductRepository(_db);
     }
 
     public void Save()
